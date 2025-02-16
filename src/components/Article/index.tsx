e41fa9144e55d834ff9976ca74105514dc0c42c7
KIM Kangseong <kim.kangseong@gmail.com>
2025-02-16 09:32:42 +0900

    250215 작성. 스타일 수정. 상세 페이지 다음글/이전글/목록 링크 추가. (#9)
    * 스타일 수정
    
    * 이전글/다음글 링크 추가, 관련 스타일 수정
    
    * 기존글 형식 수정
    
    * 250215 작성, 스타일 수정

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -6,21 +6,22 @@ import styles from './index.module.css';
 
 type Props = {
   className?: string;
-  data: Post
+  data: Post;
 }
 const Article = ({ className: _className, data }: Props) => {
   const className = `${styles.post}${_className ? ` ${_className}` : ''}`;
 
-  const year = data.date.slice(0, 4);
+  const year = +data.date.slice(0, 4) % 100;
   const month = data.date.slice(5, 7);
   const day = data.date.slice(8);
+  const dateString = `${year}${month}${day}`;
 
   return (
     <article className={className} key={data.id}>
       <h2>
         <Link className={styles.postTitleRow} href={`/posts/${data.id}`}>
           <div className={styles.postDate}>
-            {`${year}\n${month}${day}`}
+            {dateString}
           </div>
           <div className={styles.postTitle}>
             {data.title}
